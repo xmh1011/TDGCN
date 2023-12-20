37d5b6605d28b63caca336a8161f5d2201bd8d21
xmh1011 <1197843839@qq.com>
2023-12-20 17:15:21 +0800

    feat: add comment
    Signed-off-by: xmh1011 <1197843839@qq.com>

--- networks.py
+++ networks.py
@@ -14,6 +14,7 @@ class PowerLayer(nn.Module):
     """
     The power layer: calculates the log-transformed power of the data
     """
+
     def __init__(self, dim, length, step):
         super(PowerLayer, self).__init__()
         self.dim = dim
@@ -27,7 +28,7 @@ class LGGNet(nn.Module):
     def temporal_learner(self, in_chan, out_chan, kernel, pool, pool_step_rate):
         return nn.Sequential(
             nn.Conv2d(in_chan, out_chan, kernel_size=kernel, stride=(1, 1)),
-            PowerLayer(dim=-1, length=pool, step=int(pool_step_rate*pool))
+            PowerLayer(dim=-1, length=pool, step=int(pool_step_rate * pool))
         )
 
     def __init__(self, num_classes, input_size, sampling_rate, num_T,
@@ -40,9 +41,8 @@ class LGGNet(nn.Module):
         self.channel = input_size[1]
         self.brain_area = len(self.idx)
 
-        # by setting the convolutional kernel being (1,lenght) and the strids being 1, we can use conv2d to
-        # achieve the 1d convolution operation.
-        # TODO
+        # by setting the convolutional kernel being (1,lenght) and the strids being 1，we can use conv2d to
+        # achieve the 1d convolution operation
         self.Tception1 = self.temporal_learner(input_size[0], num_T,
                                                (1, int(self.window[0] * sampling_rate)),
                                                self.pool, pool_step_rate)
@@ -53,19 +53,19 @@ class LGGNet(nn.Module):
                                                (1, int(self.window[2] * sampling_rate)),
                                                self.pool, pool_step_rate)
         self.BN_t = nn.BatchNorm2d(num_T)
-        self.BN_s = nn.BatchNorm2d(num_T)
-        self.BN_fusion = nn.BatchNorm2d(num_T) # TODO: add fusion layer
-        # self.fusion_layer = self.conv_block(num_T, num_T, (3, 1), 1, 4)
         self.OneXOneConv = nn.Sequential(
             nn.Conv2d(num_T, num_T, kernel_size=(1, 1), stride=(1, 1)),
             nn.LeakyReLU(),
             nn.AvgPool2d((1, 2))
         )
-        # diag(W) to assign a weight to each local area
+        # diag(W) to assign a weight to each local areas
         size = self.get_size_temporal(input_size)
+        # 表示局部滤波器的权重。它被定义为一个形状为(self.channel, size[-1])的浮点型张量，并设置为需要梯度计算（requires_grad=True）
         self.local_filter_weight = nn.Parameter(torch.FloatTensor(self.channel, size[-1]),
                                                 requires_grad=True)
+        # 用来对local_filter_weight进行初始化，采用的是Xavier均匀分布初始化方法
         nn.init.xavier_uniform_(self.local_filter_weight)
+        # 表示局部滤波器的偏置。它被定义为一个形状为(1, self.channel, 1)的浮点型张量，初始值为全零，并设置为需要梯度计算
         self.local_filter_bias = nn.Parameter(torch.zeros((1, self.channel, 1), dtype=torch.float32),
                                               requires_grad=True)
 
@@ -73,21 +73,20 @@ class LGGNet(nn.Module):
         self.aggregate = Aggregator(self.idx)
 
         # trainable adj weight for global network
+        # 表示全局邻接矩阵。它被定义为浮点型张量，并设置为需要梯度计算（requires_grad=True）
         self.global_adj = nn.Parameter(torch.FloatTensor(self.brain_area, self.brain_area), requires_grad=True)
+        # 根据给定的张量的形状和分布进行参数初始化。用来对global_adj进行初始化，采用的是Xavier均匀分布初始化方法。
         nn.init.xavier_uniform_(self.global_adj)
         # to be used after local graph embedding
         self.bn = nn.BatchNorm1d(self.brain_area)
-        self.bn_ = nn.BatchNorm1d(self.brain_area)
         # learn the global network of networks
         self.GCN = GraphConvolution(size[-1], out_graph)
 
-        self.fc = nn.Sequential( # 组合神经网络模块
+        self.fc = nn.Sequential(  # 组合神经网络模块
             nn.Dropout(p=dropout_rate),
-            nn.ReLU(),
             nn.Linear(int(self.brain_area * out_graph), num_classes)
         )
 
-    # 该模块没有被使用
     def forward(self, x):
         y = self.Tception1(x)
         out = y
@@ -97,7 +96,7 @@ class LGGNet(nn.Module):
         out = torch.cat((out, y), dim=-1)
         out = self.BN_t(out)
         out = self.OneXOneConv(out)
-        out = self.BN_s(out)
+        out = self.BN_t(out)
         out = out.permute(0, 2, 1, 3)
         out = torch.reshape(out, (out.size(0), out.size(1), -1))
         out = self.local_filter_fun(out, self.local_filter_weight)
@@ -105,7 +104,7 @@ class LGGNet(nn.Module):
         adj = self.get_adj(out)
         out = self.bn(out)
         out = self.GCN(out, adj)
-        out = self.bn_(out)
+        out = self.bn(out)
         out = out.view(out.size()[0], -1)
         out = self.fc(out)
         return out
@@ -121,7 +120,7 @@ class LGGNet(nn.Module):
         out = torch.cat((out, z), dim=-1)
         out = self.BN_t(out)
         out = self.OneXOneConv(out)
-        out = self.BN_s(out)
+        out = self.BN_t(out)
         out = out.permute(0, 2, 1, 3)
         out = torch.reshape(out, (out.size(0), out.size(1), -1))
         size = out.size()
@@ -133,18 +132,31 @@ class LGGNet(nn.Module):
         return x
 
     def get_adj(self, x, self_loop=True):
+        """
+        x：输入的特征矩阵，大小为(b, node, feature)，其中b为批次大小，node为节点数目，feature为每个节点的特征向量维度。
+        self_loop：一个布尔值，表示是否在邻接矩阵中加入自环（自己到自己的连接）。
+        """
         # x: b, node, feature
-        adj = self.self_similarity(x)   # b, n, n
+        # 利用模型中的self_similarity方法计算输入特征矩阵x的自相似度矩阵。结果为一个大小为(b, n, n)的张量，其中n为节点数目
+        adj = self.self_similarity(x)  # b, n, n
         num_nodes = adj.shape[-1]
+        # 将自相似度矩阵与全局邻接矩阵的和进行逐元素相乘，并经过ReLU激活函数处理。这一步可以用来控制邻接矩阵中的连接关系
         adj = F.relu(adj * (self.global_adj + self.global_adj.transpose(1, 0)))
         if self_loop:
+            # 在邻接矩阵中添加自环，即在对角线上设置为1，表示每个节点与自己相连接
             adj = adj + torch.eye(num_nodes).to(DEVICE)
+        # 计算邻接矩阵每一行的元素之和，得到一个大小为(b, n)的张量
         rowsum = torch.sum(adj, dim=-1)
+        # 创建一个与rowsum大小相同的全零张量mask，并将rowsum中和为0的位置置为1。这一步是为了处理邻接矩阵中存在度为0的节点，避免除以0的错误
         mask = torch.zeros_like(rowsum)
         mask[rowsum == 0] = 1
+        # 将mask添加到rowsum中，实现对邻接矩阵的修正。避免除以0的错误，并保证每个节点的度至少为1
         rowsum += mask
+        # 计算度矩阵的逆平方根
         d_inv_sqrt = torch.pow(rowsum, -0.5)
+        # 将逆平方根得到的张量转换为对角矩阵
         d_mat_inv_sqrt = torch.diag_embed(d_inv_sqrt)
+        # 通过矩阵乘法和广播机制，将度矩阵的逆平方根与邻接矩阵相乘，得到归一化后的邻接矩阵
         adj = torch.bmm(torch.bmm(d_mat_inv_sqrt, adj), d_mat_inv_sqrt)
         return adj
 
@@ -153,17 +165,6 @@ class LGGNet(nn.Module):
         x_ = x.permute(0, 2, 1)
         s = torch.bmm(x, x_)
         return s
-
-    def compute_l2_regularization(self):
-        l2_reg = torch.tensor(0.0).to(DEVICE)
-        for param in self.parameters():
-            l2_reg += torch.norm(param, p=2)  # 计算每个可优化参数的L2范数
-        return l2_reg
-
-    def loss_fn(self, logits, labels):
-        l2_reg = self.compute_l2_regularization()  # 计算L2正则化项
-        loss = F.cross_entropy(logits, labels) + self.weight_decay * l2_reg  # 将L2正则化项添加到损失函数中
-        return loss
 
 
 class Aggregator():
