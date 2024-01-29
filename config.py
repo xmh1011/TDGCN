6358ddf8cac4c1f0e14b8632b3a117a638d6d1cd
Zohix1 <116725645+Zohix1@users.noreply.github.com>
2024-01-29 15:57:07 +0800

    2024.1.26 (#5)
    * Update config.py
    
    * Update result.txt
    
    * 数据处理文件
    
    跳过第21组数据
    
    * Update cross_validation.py
    
    跳过第21组数据
    
    * Update prepare_data.py
    
    * Update prepare_data.py
    
    * Update prepare_data.py
    
    * Update prepare_data.py
    
    ---------
    
    Co-authored-by: Zohix <https://gitee.com/zohix/test.git>

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ def set_config():
     # Data
     parser.add_argument('--dataset', type=str, default='DEAP')
     parser.add_argument('--data-path', type=str, default='/home/xiaominghao/eeg-1000-shuffled/')
-    parser.add_argument('--subjects', type=int, default=19)
+    parser.add_argument('--subjects', type=int, default=32)
     parser.add_argument('--num-class', type=int, default=2, choices=[2, 3, 4])
-    parser.add_argument('--label-type', type=str, default='V', choices=['A', 'V', 'D', 'L'])
+    parser.add_argument('--label-type', type=str, default='A', choices=['A', 'V', 'D', 'L'])
     parser.add_argument('--segment', type=int, default=4)  # segment length in seconds
     parser.add_argument('--overlap', type=float, default=0)
     parser.add_argument('--sampling-rate', type=int, default=1000)
@@ -21,11 +21,11 @@ def set_config():
     parser.add_argument('--fold', type=int, default=10)
     parser.add_argument('--random-seed', type=int, default=2023)
     parser.add_argument('--max-epoch', type=int, default=200)
-    parser.add_argument('--patient', type=int, default=8)  # 早停 最开始为20
+    parser.add_argument('--patient', type=int, default=6)  # 早停 最开始为20
     parser.add_argument('--patient-cmb', type=int, default=2)  # 原始值为8
     parser.add_argument('--max-epoch-cmb', type=int, default=10)  # 最大迭代次数 原始值为20
     parser.add_argument('--batch-size', type=int, default=25)
-    parser.add_argument('--learning-rate', type=float, default=1e-5)  # 学习率 原始值为1e-3
+    parser.add_argument('--learning-rate', type=float, default=1e-6)  # 学习率 原始值为1e-3
     parser.add_argument('--training-rate', type=float, default=0.8)
     parser.add_argument('--weight-decay', type=float, default=0.001)  # 权重衰减
     parser.add_argument('--step-size', type=int, default=5)
@@ -44,7 +44,7 @@ def set_config():
     parser.add_argument('--pool-step-rate', type=float, default=0.25)
     parser.add_argument('--T', type=int, default=64)
     parser.add_argument('--graph-type', type=str, default='gen', choices=['fro', 'gen', 'hem', 'BL'])
-    parser.add_argument('--hidden', type=int, default=32) # 隐藏层
+    parser.add_argument('--hidden', type=int, default=32)  # 隐藏层
 
     # Reproduce the result using the saved model
     parser.add_argument('--reproduce', action='store_true', default=False)
